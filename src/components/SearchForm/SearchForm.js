ed3e7b40e4006cc9c3358df2d1d308214fad49d1
Viktoria Polischuk <mrs.polischuk@yandex.ru>
2023-08-21 02:35:49 +0300

    внести правки согласно ревью

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,15 +3,17 @@ import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 function SearchForm() {
   return (
-    <form className="search-form">
-      <div className="search-form__field">
-        <input className="search-form__input" placeholder="Фильм" required />
-        <button className="search-form__button" type="submit" aria-label="Искать" />
-      </div>
-      <div className="search-form__checkbox-container">
-        <FilterCheckbox />
-      </div>
-    </form>
+    <section>
+      <form className="search-form">
+        <div className="search-form__field">
+          <input className="search-form__input" placeholder="Фильм" required />
+          <button className="search-form__button" type="submit" aria-label="Искать" />
+        </div>
+        <div className="search-form__checkbox-container">
+          <FilterCheckbox />
+        </div>
+      </form>
+    </section>
   );
 }
 
